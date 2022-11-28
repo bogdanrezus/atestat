8a45c4b73a0837795fe0361987585272c5348237
Bogdan Rezus <bogdan.rezus@gmail.com>
2022-11-28 22:43:27 +0200

    Import fonts in head tag

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,6 +6,12 @@ export default function Layout({ children }) {
     <div className="mx-auto max-w-5xl">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       <main>{children}</main>
