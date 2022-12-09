a28c20b03541a0896b31dd73588eb1347aadc957
Bogdan Rezus <bogdan.rezus@gmail.com>
2022-12-09 21:33:51 +0200

    Add code highlighting and math support

--- components/layout.jsx
+++ components/layout.jsx
@@ -12,6 +12,16 @@ export default function Layout({ children }) {
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/obsidian.min.css"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+          crossorigin="anonymous"
+        ></link>
       </Head>
       <Header />
       <main>{children}</main>
